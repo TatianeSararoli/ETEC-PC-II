484478484f593980d014b9f4bcff1be76bad2753
Tatiane Sararoli <36748975+TatianeSararoli@users.noreply.github.com>
2018-07-27 20:49:06 -0300

    Add files via upload

---
+++ 20180727-Exemplo_Banco_de_Dados/src/view/AlunosExec.java
@@ -0,0 +1,30 @@
+package view;
+
+import java.sql.Connection;
+
+import controller.AlunosJdbcDAO;
+import controller.JdbUtil;
+import model.Alunos;
+
+public class AlunosExec {
+
+	public static void main(String[] args) {
+		Alunos alunos = new Alunos();
+		try {
+			
+			alunos.setNome("Tatiane");
+			alunos.setEndereco("Av. Águia de Haia");
+			alunos.setBairro("Jd. cotinha");
+			
+			Connection connection = JdbUtil.getConnection();
+			AlunosJdbcDAO alunosJdbcDao = new AlunosJdbcDAO(connection);
+			
+			alunosJdbcDao.salvar(alunos);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
