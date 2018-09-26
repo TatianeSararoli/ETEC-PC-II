1dcf58916780763d89a1be3938ea53c05f9c9bae
Tatiane Sararoli <tatiane.souza113@etec.sp.gov.br>
2018-09-25 22:43:49 -0300

    Add files via upload

---
+++ 20180925-Projeto_Crud_Hibernate/projetoAulaCrudHibernate/projetoAulaCrudHibernate/src/main/java/view/App.java
@@ -0,0 +1,23 @@
+package view;
+
+import controller.ClienteJpaDAO;
+import model.Cliente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	Cliente cliente = new Cliente();
+        cliente.setCpf("999.999.999-99");
+        cliente.setId(1);
+        cliente.setNome("Tatiane Sararoli");
+        cliente.setRg("99.999.999-9");
+        ClienteJpaDAO.getInstance().merge(cliente);
+        System.out.println("Objetos salvo com sucesso!!!");
+    }
+}
